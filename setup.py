fd622fe544756784b8fed11cb9f36099cf282751
Jacob Hummel <jacob.a.hummel@gmail.com>
2018-09-05 14:01:41 -0600

    Cleanup extra noise in setup.py, but keep example.

--- setup.py
+++ setup.py
@@ -10,17 +10,12 @@ import os
 from setuptools import setup
 from setuptools.config import read_configuration
 
-def set_version():
+def grab_version():
     here = os.path.abspath(os.path.dirname(__file__))
     config = read_configuration(os.path.join(here,'setup.cfg'))
-    version = config['metadata']['version']
-    try:
-        branch_name = os.environ['BRANCH_NAME']
-        if branch_name != "dev":
-            version = version.replace("dev", "dev-"+branch_name)
-    except KeyError:
-        pass
-    return version
+    print("Version:", config['metadata']['version'])
 
-# Run build.
-setup(version=set_version())
+# Run setup.
+if __name__ == '__main__':
+    grab_version()
+    setup()
